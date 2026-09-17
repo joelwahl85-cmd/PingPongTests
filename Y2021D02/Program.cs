68ab7fc8d6551b7bb47455985c36b4f0b035f7f9
TheSeventh7th <mikael.smile@hotmail.se>
2026-09-17 13:47:46 +0200

    Tomte Inputs

--- Y2021D02/Program.cs
+++ Y2021D02/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace Y2021D02
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode.Common;
+using Y2021D01;
+
+Console.WriteLine("Hello, World!");
+var path = Path.Combine(AppContext.BaseDirectory, "input.txt");
+var text = File.ReadAllText(path);
+
+var tomte = new Tomte();
+var floor_sum = tomte.CountTomteFloor(text);
+
+Console.WriteLine($"The file contained tomte who is at floor {floor_sum}");
---
+++ Y2021D02/Tomte.cs
@@ -0,0 +1,20 @@
+﻿namespace Y2021D02
+{
+    public class Tomte
+    {
+        public int CountTomteFloor(string[] floors){
+			int _floor = 0;
+	
+			foreach	(var direction in floor){
+				if (direction == ")"){
+					_floor--;
+				}
+				else if (direction == "("){
+					_floor++;
+				}
+			}
+			
+			return _floor;
+		}
+    }
+}
